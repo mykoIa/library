ba8b40776904beb653faf089db0f854456a61757
mykoIa <gitPasswordKnowAllButNotMe0089>
2021-03-24 10:51:18 +0200

    Init commit. Add model User and Book. Add interface BookRepository

---
+++ src/main/java/ua/sukhorutchenko/library/model/Book.java
@@ -0,0 +1,46 @@
+package ua.sukhorutchenko.library.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ua/sukhorutchenko/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package ua.sukhorutchenko.library.repository;
+
+
+import ua.sukhorutchenko.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
