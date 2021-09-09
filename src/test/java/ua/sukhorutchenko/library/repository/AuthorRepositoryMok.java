60762ef289bf2f8e2246fd193de8d103cb993647
Mykola Sukhorutchenko <m.sukhorutchenko@infostroy.com.ua>
2021-09-09 15:18:17 +0300

    refactor code

---
+++ src/test/java/ua/sukhorutchenko/library/repository/AuthorRepositoryMok.java
@@ -0,0 +1,8 @@
+package ua.sukhorutchenko.library.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AuthorRepositoryMok {
+}
