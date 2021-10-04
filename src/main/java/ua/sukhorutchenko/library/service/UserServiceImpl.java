e8cf4dddbef40a134030fabe04da83680a1aa3d8
Mykola Sukhorutchenko <m.sukhorutchenko@infostroy.com.ua>
2021-10-04 11:35:01 +0300

    add service

--- src/main/java/ua/sukhorutchenko/library/service/UserServiceImpl.java
+++ src/main/java/ua/sukhorutchenko/library/service/UserServiceImpl.java
@@ -1,35 +1,49 @@
 package ua.sukhorutchenko.library.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.sukhorutchenko.library.dto.UserDTO;
+import ua.sukhorutchenko.library.mapper.UserMapper;
+import ua.sukhorutchenko.library.repository.UserRepository;
 import ua.sukhorutchenko.library.service.interf.UserService;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
+@Service
 public class UserServiceImpl implements UserService {
 
-    @Override
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Transactional
     public List<UserDTO> findAllUser() {
-        return null;
+        return userMapper.entityToDTO(userRepository.findAll());
     }
 
-    @Override
+    @Transactional
     public UserDTO findUserById(Long id) {
-        return null;
+        return userMapper.entityToDTO(userRepository.findById(id).orElseThrow(NoSuchElementException::new));
     }
 
-    @Override
+    @Transactional
     public void addUser(UserDTO user) {
-
+        userRepository.save(userMapper.dtoToEntity(user));
     }
 
-    @Override
+    @Transactional
     public void deleteUserById(Long id) {
-
+        userRepository.deleteById(id);
     }
 
-    @Override
+    @Transactional
     public void updateUser(UserDTO user) {
-
+        userRepository.save(userMapper.dtoToEntity(user));
     }
 
 }
